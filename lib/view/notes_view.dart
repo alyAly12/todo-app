e18f9f64b1a04850a02c3c6d33447dab21c92355
aly <medoaly870@gmail.com>
2023-07-31 18:52:34 +0300

    adding color picker to side drawer

--- lib/view/notes_view.dart
+++ lib/view/notes_view.dart
@@ -1,35 +1,47 @@
+
 import 'package:flutter/material.dart';
 import 'package:todo_task/view/widgets/notes_view_body.dart';
+import 'package:todo_task/view/widgets/side_drawer.dart';
 
-class NotesView extends StatelessWidget {
+
+
+class NotesView extends StatefulWidget {
   const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      endDrawer: const MyDrawer(),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(24),
         ),
-          onPressed: (){},
-        child: const Icon(Icons.add,color: Colors.white,),
+          onPressed: (){
+
+          },
+        child: const Icon(Icons.add,color: Colors.white,size: 30,),
       ),
       appBar: AppBar(
-        elevation: 0,
+        elevation: 0.0,
+        shadowColor: Theme.of(context).scaffoldBackgroundColor,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         centerTitle: true,
         title: const Text(
           'TODO',
           style: TextStyle(fontSize: 30,
               fontWeight: FontWeight.normal),
         ),
-        actions: [
-          IconButton(
-              onPressed: (){},
-              icon: const Icon(Icons.list))
-        ],
+
       ),
       body: const NotesViewBody(),
     );
   }
 }
+
---
+++ lib/view/widgets/side_drawer.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'color_picker_widget.dart';
+
+class MyDrawer extends StatefulWidget {
+  const MyDrawer({super.key});
+
+  @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  final List<Color> color=[
+    Colors.pink,
+    Colors.lightBlue,
+    Colors.pinkAccent,
+    Colors.blueGrey,
+    Colors.green,
+    Colors.yellow,
+  ];
+  late int selectedIndex ;
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      width: MediaQuery.of(context).size.width*0.8,
+      child:   Padding(
+        padding:  const EdgeInsets.symmetric(vertical: 26,horizontal: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'New Task',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.normal
+              ),
+            ),
+            const SizedBox(height: 20,),
+            const Text(
+              'Color',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.grey
+              ),
+            ),
+            const SizedBox(height: 20,),
+            SizedBox(
+              width: double.infinity,
+              height: 80,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: color.length,
+                  itemBuilder: (context,index){
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ColorPickerWidget(color: color[index], fun: () {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  } ,),
+                );
+              }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
